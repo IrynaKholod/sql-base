56c7ef5262e4099477533e20088b43a94ec9fafd
IrynaKholod <104202392+IrynaKholod@users.noreply.github.com>
2024-07-26 21:16:06 +0300

    Add SQL scripts for movie application database

--- Queries/6_detailed_movie_info_by_id.sql
+++ Queries/6_detailed_movie_info_by_id.sql
@@ -1,45 +1,45 @@
 SELECT
-    m.id AS "ID",
-    m.title AS "Title",
-    m.releaseDate AS "Release Date",
-    m.duration AS "Duration",
-    m.description AS "Description",
+    movies.id AS "ID",
+    movies.title AS "Title",
+    movies.releaseDate AS "Release Date",
+    movies.duration AS "Duration",
+    movies.description AS "Description",
     json_build_object(
-        'fileName', uf.fileName,
-        'mimeType', uf.mimeType,
-        'url', uf.url
+        'fileName', user_files.fileName,
+        'mimeType', user_files.mimeType,
+        'url', user_files.url
     ) AS "Poster",
     json_build_object(
-        'id', p.id,
-        'firstName', p.firstName,
-        'lastName', p.lastName,
+        'id', persons.id,
+        'firstName', persons.firstName,
+        'lastName', persons.lastName,
         'photo', json_build_object(
-            'fileName', pf.fileName,
-            'mimeType', pf.mimeType,
-            'url', pf.url
+            'fileName', person_files.fileName,
+            'mimeType', person_files.mimeType,
+            'url', person_files.url
         )
     ) AS "Director",
     (SELECT json_agg(
         json_build_object(
-            'id', a.id,
-            'firstName', a.firstName, 
-            'lastName', a.lastName,
+            'id', actors.id,
+            'firstName', actors.firstName, 
+            'lastName', actors.lastName,
             'photo', json_build_object(
-                'fileName', af.fileName,
-                'mimeType', af.mimeType,
-                'url', af.url
+                'fileName', actor_files.fileName,
+                'mimeType', actor_files.mimeType,
+                'url', actor_files.url
             )
         )
     )
-    FROM PERSONS a
-    LEFT JOIN person_files af ON af.id = a.primaryPhotoId
-    INNER JOIN characters c ON a.id = c.personId
-    WHERE c.movieId = m.id) AS "Actors",
-    m.genres AS "Genres" -- Corrected column name
+    FROM persons actors
+    LEFT JOIN person_files actor_files ON actor_files.id = actors.primaryPhotoId
+    INNER JOIN characters ON actors.id = characters.personId
+    WHERE characters.movieId = movies.id) AS "Actors",
+    movies.genres AS "Genres"
 FROM
-    movies m
-LEFT JOIN user_files uf ON uf.id = m.posterId
-LEFT JOIN persons p ON p.id = m.directorId
-LEFT JOIN person_files pf ON pf.id = p.primaryPhotoId
+    movies
+LEFT JOIN user_files ON user_files.id = movies.posterId
+LEFT JOIN persons ON persons.id = movies.directorId
+LEFT JOIN person_files ON person_files.id = persons.primaryPhotoId
 WHERE 
-    m.id = 1;
+    movies.id = 1;
